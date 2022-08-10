8e5909c9aa9cb26045ba4e932404af9aca8866ad
Irfnd <irfandiabimanyu@gmail.com>
2022-08-10 20:43:10 +0700

    Update - Mutate on search change

--- components/lists/SearchRecipesList.js
+++ components/lists/SearchRecipesList.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import useSWR from "swr";
 import recipesWrapper from "@/utils/axios/recipesWrapper";
 
@@ -5,7 +6,9 @@ import recipesWrapper from "@/utils/axios/recipesWrapper";
 import RecipesList from "@/components/card/RecipesList";
 
 export default function SearchRecipesList({ name }) {
-	const { data } = useSWR("/recipes", (url) => recipesWrapper.getByName(url, name));
+	const { data, mutate } = useSWR("/recipes", (url) => recipesWrapper.getByName(url, name));
+
+	useEffect(() => mutate(), [name, mutate]);
 
 	return <>{data?.length > 0 && data?.map((recipe) => <RecipesList recipe={recipe} type="search" key={recipe.id} withBtn />)}</>;
 }
